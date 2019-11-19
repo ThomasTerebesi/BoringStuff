17ed3c72c65461178282654b3afd35778db624ec
Thomas <thomasterebesi@googlemail.com>
2019-11-19 23:13:12 +0100

    Finished chapter 01.

---
+++ Chapter 01/hello.py
@@ -0,0 +1,14 @@
+# This program says hello and asks for the user's name and the user's age.
+
+print('Hello World!')
+
+print('\nWhat is your name?')
+myName = input()
+print('\nIt is good to meet you, ' + myName)
+
+print('The length of your name is: ' + str(len(myName)))
+
+print('\nWhat is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge)) + 1) + ' years old soon.')
+
