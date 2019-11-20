541d04ff880615f67521c017937a360b6360626c
Thomas <thomasterebesi@googlemail.com>
2019-11-20 22:33:27 +0100

    Finished chapter 02.

---
+++ Chapter 02/fiveTimes.py
@@ -0,0 +1,12 @@
+print('For loop version:')
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+
+print('\n\nWhile loop version:')
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
---
+++ Chapter 02/flowControl.py
@@ -0,0 +1,8 @@
+someVariable = int(input())
+
+if someVariable < 10:
+    print('First block executed.')
+elif someVariable > 10:
+    print('Second block executed.')
+else: 
+    print('Third block executed.')
